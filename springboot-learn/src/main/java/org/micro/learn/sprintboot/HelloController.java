a27a2a0cd15826737227cbae4c48328f8cb96113
Rabbit Xiao (rxiao) <Rabbit.Xiao@quest.com>
2021-03-31 14:25:03 +0800

    First commit, add springboot-learn project.

---
+++ springboot-learn/src/main/java/org/micro/learn/sprintboot/HelloController.java
@@ -0,0 +1,14 @@
+package org.micro.learn.sprintboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String getHello() {
+        return "hello";
+    }
+}
