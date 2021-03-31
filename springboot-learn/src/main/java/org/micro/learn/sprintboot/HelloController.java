98f4b6cc6ee826bfb7927a77ec32ac786b829b27
Rabbit Xiao (rxiao) <Rabbit.Xiao@quest.com>
2021-03-31 14:49:01 +0800

    Annotation1 - Add java annotation for jdbc properties file.

--- springboot-learn/src/main/java/org/micro/learn/sprintboot/HelloController.java
+++ springboot-learn/src/main/java/org/micro/learn/sprintboot/HelloController.java
@@ -1,11 +1,17 @@
 package org.micro.learn.sprintboot;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.sql.DataSource;
+
+@RestController
 public class HelloController {
+    @Autowired
+    private DataSource dataSource;
+
     @RequestMapping("/")
     @ResponseBody
     public String getHello() {
